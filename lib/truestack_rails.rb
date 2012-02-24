69b0966095cc3ac5f0452f97d8d3bfcd0cee46b0
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-02-24 10:31:29 -0500

    update

--- lib/truestack_rails.rb
+++ lib/truestack_rails.rb
@@ -1,3 +1,27 @@
+require 'truestack_rails/config'
+require 'truestack_rails/railtie_3_0'
+require 'truestack_rails/railtie_3_1'
+require 'truestack_rails/railtie_3_2'
+
 module TruestackRails
-  # Your code goes here...
+  class Railtie < ::Rails::Railtie
+    config.before_initialize do
+      TruestackRails.init_rails(binding)
+    end
+  end
+
+  ## Attach to all of the different events based
+  # on what rails version you are
+  def self.init_rails(binding)
+    case (::Rails.version.to_f * 10.0).to_i / 10.0
+    when 3.0
+      TruestackRails::Railtie30.connect!
+    when 3.1
+      TruestackRails::Railtie31.connect!
+    when 3.2
+      TruestackRails::Railtie32.connect!
+    else
+      raise "Truestack does not support this version of Rails"
+    end
+  end
 end
