7b67e424ec3f5e2bb42ecf3430a494d09bf7af7d
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-04-04 15:33:55 -0400

    updated

--- lib/truestack_rails.rb
+++ lib/truestack_rails.rb
@@ -80,7 +80,7 @@ module TruestackRails
       retval = nil
       ActiveSupport::Notifications.instrument("truestack.method_call", :klass=>self, :method=>:#{method}, :location=>'#{location}') do
 ::Rails.logger.info("Inside wrapped method call!")
-#{do_class_eval ? '' : 'binding.pry'}
+#{do_class_eval ? 'binding.pry' : ''}
         if block_given?
           retval = #{WRAPPED_METHOD_PREFIX}_#{method}(*args, &block)
         else
