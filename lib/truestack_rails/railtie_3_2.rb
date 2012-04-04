0064333183e115cca935901f4573b5db611951ca
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-04-04 13:12:53 -0400

    updated

--- lib/truestack_rails/railtie_3_2.rb
+++ lib/truestack_rails/railtie_3_2.rb
@@ -40,8 +40,8 @@ module TruestackRails
         results = TruestackRails.track_methods_results
         TruestackRails.reset_methods
 
-        TruestackClient.logger.info( "#{args[:controller_name]}##{args[:action_name]} #{stime.to_i}, #{results}")
-        TruestackClient.request("#{args[:controller_name]}##{args[:action_name]}", stime.to_i, results)
+        TruestackClient.logger.info( "#{args[:controller_name]}##{args[:action_name]} #{tstart.to_i}, #{tend.to_i}, #{results}")
+        TruestackClient.request("#{args[:controller_name]}##{args[:action_name]}", tstart.to_i, results)
       end
     end
   end
