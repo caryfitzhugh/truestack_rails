f28555086980a80e2cad140b635a27af291ee257
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-04-05 14:36:17 -0400

    updated / refactoring

--- lib/truestack_rails/browser_tracking.rb
+++ lib/truestack_rails/browser_tracking.rb
@@ -1,7 +1,6 @@
 module TruestackRails
   module BrowserTracking
     def  truestack_browser_tracker
-      binding.pry
       img_url = URI.parse(TruestackClient.config.host)
       img_url.path = "/app/event"
       img_url.query = "TrueStack-Access-Key=#{TruestackClient.config.key}&type='browser'"
@@ -16,7 +15,7 @@ var _truestack_browser_data = {
 
 window.onload = function() {
   _truestack_browser_data.tloaded = new Date();
-  if (_truestack_browser_data.old_onload()) {
+  if (_truestack_browser_data.old_onload) {
     _truestack_browser_data.old_onload();
   }
   _truestack_browser_data.tready = new Date();
