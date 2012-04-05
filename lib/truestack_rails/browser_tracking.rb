63645ba23ffe778b4f22a588d3b3dec59e7c2c92
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-04-05 15:33:41 -0400

    updated / refactoring

--- lib/truestack_rails/browser_tracking.rb
+++ lib/truestack_rails/browser_tracking.rb
@@ -2,8 +2,8 @@ module TruestackRails
   module BrowserTracking
     def  truestack_browser_tracker
       img_url = URI.parse(TruestackClient.config.host)
-      img_url.path = "/app/event"
-      img_url.query = "TrueStack-Access-Key=#{TruestackClient.config.key}&type=browser"
+      img_url.path = "/app/browser_event"
+      img_url.query = "TrueStack-Access-Key=#{TruestackClient.config.key}"
       return <<JS
 <script>
 // Truestack.com
@@ -26,12 +26,13 @@ window.onload = function() {
 
   var newimg = document.createElement('img');
   newimg.setAttribute("style", "height:1px; width:1px");
-  newimg.setAttribute("src","#{img_url}&tstart="+_truestack_browser_data.tstart.getTime() / 1000.0+"&tloaded="+_truestack_browser_data.tloaded.getTime() / 1000.0+"&tready="+_truestack_browser_data.tready.getTime() / 1000.0);
+  newimg.setAttribute("src","#{img_url}"+
+    "&tstart="+ _truestack_browser_data.tstart.getTime() / 1000.0+
+    "&tloaded="+_truestack_browser_data.tloaded.getTime() / 1000.0+
+    "&tready="+_truestack_browser_data.tready.getTime() / 1000.0);
 
   document.body.appendChild(newimg);
 }
-
-
 </script>
 JS
     end
--- lib/truestack_rails/version.rb
+++ lib/truestack_rails/version.rb
@@ -1,3 +1,3 @@
 module TruestackRails
-  VERSION = "0.0.78"
+  VERSION = "0.0.80"
 end
