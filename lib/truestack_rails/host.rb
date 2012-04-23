9903a62506de2541445658938278001946153a26
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-04-23 13:25:05 -0400

    debugging

--- lib/truestack_rails/host.rb
+++ lib/truestack_rails/host.rb
@@ -2,7 +2,6 @@ require 'sys/host'
 
 module TruestackRails
   module Host
-    include Sys
     def self.report_once!
       if (!@_truestack_sent_report)
         @_truestack_sent_report = Time.now
@@ -10,16 +9,16 @@ module TruestackRails
       self.report!
     end
     def self.report!
-      #Momentarily.next_tick do
+      Momentarily.next_tick do
 binding.pry
-        TruestackClient.logger.info "VERSION: " + Host::VERSION
-        TruestackClient.logger.info "Hostname: " + Host.hostname
-        TruestackClient.logger.info "IP Addresses : " + Host.ip_addr.join(',')
-        TruestackClient.logger.info "Host ID: " + Host.host_id.to_s
+        TruestackClient.logger.info "VERSION: " + Sys::Host::VERSION
+        TruestackClient.logger.info "Hostname: " + Sys::Host.hostname
+        TruestackClient.logger.info "IP Addresses : " + Sys::Host.ip_addr.join(',')
+        TruestackClient.logger.info "Host ID: " + Sys::Host.host_id.to_s
 
         TruestackClient.logger.info "Info: "
-        TruestackClient.logger.info Host.info.to_yaml
-      #end
+        TruestackClient.logger.info Sys::Host.info.to_yaml
+      end
     end
 
   end
--- lib/truestack_rails/version.rb
+++ lib/truestack_rails/version.rb
@@ -1,3 +1,3 @@
 module TruestackRails
-  VERSION = "0.0.137"
+  VERSION = "0.0.138"
 end
