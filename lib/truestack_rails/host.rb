3290c7a1d85129e28a81cae90e8fb5b5f2791026
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-04-26 16:53:15 -0400

    work on heroku

--- lib/truestack_rails/host.rb
+++ lib/truestack_rails/host.rb
@@ -4,24 +4,28 @@ require 'grit'
 module TruestackRails
   module Host
     def self.report_once!
-      TruestackClient.logger.info "Report once! called"
-      TruestackClient.logger.info "And: #{@_truestack_sent_report}"
       if (!@_truestack_sent_report)
         @_truestack_sent_report = Time.now
         self.report!
       end
     end
+    def self.scm_version
+      # If on heroku or something else with a helpful env
+      version = ENV['COMMIT_HASH']
+      # Otherwise try this
+      if version.blank?
+        version = Grit::Repo.new(Rails.root).head.commit.id
+      end
+      version
+    end
     def self.report!
       if (TruestackRails::Configuration.environments.include?(Rails.env))
         Momentarily.next_tick do
-          TruestackClient.logger.info "Reporting..."
           begin
-            repo = Grit::Repo.new(Rails.root)
             methods = TruestackRails::Instrument.instrumented_methods
-            scm_version = repo.head.commit.id
             host_id = "#{Sys::Host.host_id.to_s}/#{Sys::Host.ip_addr.join(',')}/#{Process.pid }/#{Rails.env}"
 
-            TruestackClient.startup(scm_version, host_id, methods)
+            TruestackClient.startup(self.scm_version, host_id, methods)
           rescue Exception => e
             TruestackClient.logger.info "Exception reporting! #{e}"
             TruestackClient.logger.info "Exception backtrace! #{e.backtrace.join("\n")}"
--- lib/truestack_rails/version.rb
+++ lib/truestack_rails/version.rb
@@ -1,3 +1,3 @@
 module TruestackRails
-  VERSION = "0.0.148"
+  VERSION = "0.0.149"
 end
